a5364febcd229f33eac47f70ea5d7d8e53ee8c80
Bianca A <frank@Bianca-Avila-4.local>
2018-03-25 19:09:56 +0900

    modal boxes by mode

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-
-
 $(function() {
-	console.log("this is conected");
-	// top nav display 
-	
     // top nav to mmanage what to display when sixe changes
     const topNav = document.getElementById("myTopNav");
 
@@ -16,4 +11,50 @@ $(function() {
         }
 
     })
-})// end of j query
+
+    // modal boxes 
+    $(".portafolio").find(":button").click(function() {
+        var value = $(this).val();
+        modalBox(value);
+    })
+    // select dom elements 
+    const modal = document.getElementById("modal");
+    const span = document.getElementsByClassName("close")[0];
+    const removeList = [];
+
+    // display remover 
+    const remover = (list) => {
+        list.forEach(e => {
+            let modeRemover = modal.querySelectorAll(`.${e}`)[0];
+            modeRemover.style.display = "none"
+        });
+    }
+    // modal display 
+    const modalBox = (value) => {
+        // push values of previously selected
+        removeList.push(value);
+        modal.style.display = "block"
+        // remove prevously selected elements
+        remover(removeList);
+        const modeSelector = modal.querySelectorAll(`.${value}`)[0];
+        if (value === "front-end") {
+            modeSelector.style.display = "block"
+        } else if (value === "full-stack") {
+            modeSelector.style.display = "block"
+        } else if (value === "react") {
+            modeSelector.style.display = "block"
+        } else {
+            modeSelector.style.display = "block"
+        }
+    }
+    // close the span
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // click outside of the modal 
+    window.onclick = function(e) {
+        if (e.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}) // end of j query
