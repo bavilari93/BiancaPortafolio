f78f912a7107ccc4f623d537789626dcd1f89452
Bianca A <frank@Bianca-Avila-4.local>
2018-03-27 21:12:59 +0900

    modal css fix

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ $(function() {
     // click outside of the modal 
     window.onclick = function(e) {
     	console.log(e);
-    	console.log(span);
     	console.log("im coling the modal");
         if (e.target == modal) {
             modal.style.width = "0%";
