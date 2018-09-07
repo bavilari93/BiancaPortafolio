5f154af58ce9aeba85d937be9bfbdca85a82c12f
Bianca A <frank@Bianca-Avila-4.local>
2018-09-07 14:48:02 +0900

    paralax and animation

--- index.js
+++ index.js
@@ -1,81 +1,114 @@
-$(function() {
-    // top nav to mmanage what to display when sixe changes
-    const topNav = document.getElementById("myTopNav");
+$(() => {
 
-    $(".icon").click(function() {
-        if (topNav.className === "top-nav") {
-            topNav.className += " responsive";
-            // else is in case of unclick
-        } else {
-            topNav.className = "top-nav"
-        }
-
-    })
-
-    // modal boxes 
-    $(".button-project").click(function() {
-
-        var value = $(this).val();
-        modalBox(value);
-    })
+            // modal boxes 
+            $(".button-project").click(function() {
+                var value = $(this).val();
+                modalBox(value);
+            })
 
 
+            // select dom elements 
+            const modal = document.getElementById("modal");
+            const span = document.getElementsByClassName("close")[0];
+            const removeList = [];
 
-    // select dom elements 
-    const modal = document.getElementById("modal");
-    const span = document.getElementsByClassName("close")[0];
-    const removeList = [];
+            // display remover 
+            const remover = (list) => {
+                list.forEach(e => {
+                    let modeRemover = modal.querySelectorAll(`.${e}`)[0];
+                    modeRemover.style.display = "none"
+                });
+            }
+            // modal display 
+            const modalBox = (value) => {
+                // push values of previously selected
+                removeList.push(value);
+                modal.style.width = "100%"
 
-    // display remover 
-    const remover = (list) => {
-        list.forEach(e => {
-            let modeRemover = modal.querySelectorAll(`.${e}`)[0];
-            modeRemover.style.display = "none"
-        });
-    }
-    // modal display 
-    const modalBox = (value) => {
-        // push values of previously selected
-        removeList.push(value);
-        modal.style.width = "100%"
+                // remove prevously selected elements
 
-        // remove prevously selected elements
-        remover(removeList);
-        const modeSelector = modal.querySelectorAll(`.${value}`)[0];
-        if (value === "front-end") {
-            modeSelector.style.display = "block"
-        } else if (value === "full-stack") {
-            modeSelector.style.display = "block"
-        } else if (value === "react") {
-            modeSelector.style.display = "block"
-        } else if (value === "express") {
-            modeSelector.style.display = "block"
-        }
-    }
-    // close the span
-    span.onclick = function() {
-        modal.style.width = "0%";
-    }
-    // click outside of the modal 
-    window.onclick = function(e) {
-        if (e.target == modal) {
-            modal.style.width = "0%";
-        }
-    }
+                remover(removeList);
+                const modeSelector = modal.querySelectorAll(`.${value}`)[0];
+                if (value === "front-end") {
+                    modeSelector.style.display = "block"
+                    topNav.className = 'top-nav'
+                } else if (value === "full-stack") {
+                    modeSelector.style.display = "block"
+                } else if (value === "react") {
+                    modeSelector.style.display = "block"
+                } else if (value === "express") {
+                    modeSelector.style.display = "block"
+                }
+            }
+            // close the span
+            span.onclick = function() {
+                modal.style.width = "0%";
+            }
+            // click outside of the modal 
+            window.onclick = function(e) {
+                if (e.target == modal) {
+                    modal.style.width = "0%";
+                }
+            }
+            // topnav mobile 
+            const topNav = document.getElementById("myTopNav");
+            const container = document.querySelector('.modal-container');
+            $('.icon').click(() => {
+                if (topNav.className === 'top-nav') {
+                    topNav.className += " responsive";
+                    container.style.display = 'block';
+                    $('.icon').toggleClass("change");
 
-    // section transition
-    window.addEventListener('scroll', function(){
-        // appear on scroll effect
-        const pageTop = $(this).scrollTop()
-        const pageBottom = pageTop + $(this).height()
-        var tags = $('section')
-         for (var i = 0; i < tags.length; i++) {
-                        let tag = tags[i]
+                } else {
+                    topNav.className = 'top-nav'
+                    container.style.display = 'none';
+                    $(".icon").removeClass('change');
+                }
+            })
+            // close when click on a top-nav 
+            container.addEventListener('click', function(e){
+                console.log('click');
+                topNav.className = 'top-nav'
+                 container.style.display = 'none';
+                 $(".icon").removeClass('change');
+            })
+            
+
+
+
+                const parallax = document.getElementById('parallax');
+                window.addEventListener('scroll', function() {
+                    // paralax
+                    let offset = window.pageYOffset;
+                    parallax.style.backgroundPositionY = offset * 0.4 + "px";
+                    // make each section visible when scroll
+                    let pageTop = $(this).scrollTop()
+                    let pageBottom = pageTop + $(this).height();
+                    let sectionTags = document.getElementsByTagName('section');
+                    for (var i = 0; i < sectionTags.length; i++) {
+                        let tag = sectionTags[i]
                         if ($(tag).position().top < pageBottom) {
                             $(tag).addClass('visible')
                         } else {
                             $(tag).removeClass('visible')
                         }
                     }
-    })
-}) // end of j query
+                    // fade out header content 
+                    if (pageTop > 50) {
+                        $('.tittle').fadeOut('slow');
+                        $('#parallax').css('background-image', "url()")
+                    } else {
+                        $('.tittle').fadeIn('slow');
+                        $('#parallax').css('background-image', "url(background.png)")
+                    }
+                })
+
+                // move on mouse over 
+
+                $('#parallax').mousemove(function(e) {
+                    var amountMovedX = (e.pageX * -1 / 6);
+                    var amountMovedY = (e.pageY * -1 / 6);
+                    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
+                });
+
+            }) // end of jquery
