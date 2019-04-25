084aec39baaf8a69f32aa76790add420d16766a2
bavilari93 <biancasar93@gmail.com>
2019-04-25 16:49:35 -0600

    resume

--- main.js
+++ main.js
@@ -33,7 +33,16 @@ $(() => {
 
 
 
-
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
+    
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
 
     // modal box open 
     $('button').click(function() {
